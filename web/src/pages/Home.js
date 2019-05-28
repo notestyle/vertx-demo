ab4fc499fe8651d9105f35d39183bf096675e9de
Oldokh <UguumurO.1@corp.riotinto.org>
2019-05-28 16:59:06 +0800

    remove bugs

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -14,28 +14,24 @@ class Home extends Component {
             try{
                 let response = await fetch(`https://reqres.in/api/users?page=1`);
                 let data = await response.json();
-                console.log('Response: ', data.data);
-
-                this.setState(state => {
-                    let newData = this.state.datas;
+                let newData = this.state.datas;
                     
-                    data.data.map(x=>{
-                        newData.push({
-                            id: x.id,
-                            name: x.first_name,
-                            lastName: x.last_name,
-                            role: x.avatar,
-                            createdDate: (new Date()).toDateString()
-                        })
-                    });
-                    return {
-                        newData
-                    };
+                data.data.forEach(x => {
+                    newData.push({
+                        id: x.id,
+                        name: x.first_name,
+                        lastName: x.last_name,
+                        role: x.avatar,
+                        createdDate: (new Date()).toDateString()
+                    })
                 });
+
+                this.setState({datas: newData});
                 
-                this.props.enqueueSnackbar('Getting data.', { variant: 'success'});
+                this.props.enqueueSnackbar('Data downloaded.', { variant: 'success'});
                 this.setState({isConnected : true});
             }catch(err){
+                console.log(err);
                 this.props.enqueueSnackbar('Error: ', err, { variant: 'error'});
                 this.setState({isConnected : false});
             }
