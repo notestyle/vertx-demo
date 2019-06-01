fb5e28910c80a94cbba283d7e0ecc0aa429fa72d
Ariuka <da-44@da-44s-MacBook-Air.local>
2019-06-01 14:00:11 +0800

    mai

---
+++ web/build/precache-manifest.f1c5764454e82422c2c1d2222633d1ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4093ff314275b02410964e82d36e3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3bc2cab95e7f4afe747",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "c8bde7f9907ddf6a3d30",
+    "url": "/static/js/2.1126a365.chunk.js"
+  },
+  {
+    "revision": "d3bc2cab95e7f4afe747",
+    "url": "/static/js/main.16a34806.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
